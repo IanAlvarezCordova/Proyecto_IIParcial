c2c44dd078d2bf046921cf624a8fbf8991eae8f7
Ian Joel <ijalvarez@espe.edu.ec>
2025-01-14 17:01:15 -0500

    2do

--- src/componentes/Departamentos.tsx
+++ src/componentes/Departamentos.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./GestionDepartamentos.css";
+import "./Gestion.css";
 
 interface Departamento {
   id: number;
--- src/componentes/Empleados.tsx
+++ src/componentes/Empleados.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Gestion.css";
 
 interface Departamento {
   id: number;
